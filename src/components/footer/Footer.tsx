11f0b8f51bcd9c857965e0722e08056e9d5e8733
Bowie <larissa.soaress@yahoo.com.br>
2025-06-02 11:30:42 -0300

    "Segunda etapa-finalizada"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,14 +10,15 @@ function Footer() {
             <div className="flex justify-center bg-gradient-to-r from-yellow-100 to-yellow-400  text-red-500">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            FarmaBem | Copyright: {data}
-                        </p>
+                        FarmaBem | Copyright: {data}
+                    </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-4'>
-
+                        <a href="https://github.com/LarissaSoaresSilva/projeto_final_bloco_03/tree/main" target='_blank'></a>
                         <GithubLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/larissa-soares-da-silva/" target='_blank'></a>
                         <LinkedinLogoIcon size={48} weight='bold' />
-                        
+
                     </div>
                 </div>
             </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,26 @@
+import { Link, useNavigate } from "react-router"
+
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    function logout() {
+
+        navigate('/')
+    }
+
     return (
         <>
             <div className='w-full flex justify-center py-6
             			  bg-gradient-to-r from-yellow-100 to-yellow-400  text-red-500'>
 
                 <div className="container flex justify-between text-lg">
-                    FarmaBem
+                    <Link to='/home' className="text-2xl font-bold">FarmaBem</Link>
 
-                    <div className='flex gap-6'>
-                        Home
-                        Categoria
-                        Cadastrar
-                        Sair
+                    <div className='flex gap-6 '>
+                        <Link to='/categoria' className='hover:underline'>Categoria</Link>
+                        <Link to='/cadastrar' className='hover:underline'>Cadastrar</Link>
+                        <Link to='' onClick={logout} className='hover:underline'> Sair</Link>
                     </div>
                 </div>
             </div>
